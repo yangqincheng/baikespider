9ae9a5a541f81c5b3fb73b0832bc99911e9f18d0
Ziqih <32486345+Ziqih@users.noreply.github.com>
2018-04-27 13:11:25 +0800

    Update BaiDuSpider.py
    解决图片和命名不对应的问题
    利用字典

--- scrapyspider/spiders/BaiDuSpider.py
+++ scrapyspider/spiders/BaiDuSpider.py
@@ -1,5 +1,5 @@
 from scrapy.spiders import Spider
-from scrapyspider.items import BaiKeItem
+from scrapyspider.items import BaiKeItem, PicturesItem
 from scrapy import Request
 from scrapy.selector import Selector
 from urllib import parse
@@ -7,10 +7,14 @@ import re
 import json
 from scrapyspider import pipelines
 
-import win_unicode_console
-win_unicode_console.enable()
-# Linux不用加
+# import time
 
+
+# import win_unicode_console
+
+# win_unicode_console.enable()
+
+# Linux不用加
 
 
 class BaiKeSpider(Spider):
@@ -21,14 +25,19 @@ class BaiKeSpider(Spider):
             'Mozilla/5.0 '
             '(Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
     }
-
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'scrapyspider.pipelines.BaiKeSpiderPipeline': 300
+        }
+    }
     def __init__(self):
-        sqls = pipelines.ScrapyspiderPipeline()
-        # sqls.create_entity_table('entity_table')
-        # sqls.create_polysemant_table('synonym_table')
+        self.sqls = pipelines.BaiKeSpiderPipeline()
+        # self.sqls.create_entity_table('entity_table')
+        # self.sqls.create_polysemant_table('synonym_table')
 
     def start_requests(self):
-        urls=['https://baike.baidu.com/item/%E9%B2%81%E8%BF%85/36231','https://baike.baidu.com/item/%E9%98%BF%E5%B0%94%E4%BC%AF%E7%89%B9%C2%B7%E7%88%B1%E5%9B%A0%E6%96%AF%E5%9D%A6']
+        urls = ['https://baike.baidu.com/item/%E9%B2%81%E8%BF%85/36231',
+                'https://baike.baidu.com/item/%E9%98%BF%E5%B0%94%E4%BC%AF%E7%89%B9%C2%B7%E7%88%B1%E5%9B%A0%E6%96%AF%E5%9D%A6']
         for url in urls:
             yield Request(url, headers=self.headers)
 
@@ -37,7 +46,7 @@ class BaiKeSpider(Spider):
         # sites = sel.xpath('//div[@class="main-content"]')
         item = BaiKeItem()
 
-        current_url=response.url
+        current_url = response.url
 
         p = re.compile('.*(\/item\/[^\?]+).*')  # 去掉无意义的？及后面的部分
 
@@ -82,7 +91,7 @@ class BaiKeSpider(Spider):
 
             # 提取链接
             tmp_link_list = value_node.re('\"(\/item\/[^\"]+)\"')
-            p=re.compile('(\/item\/[^\?]+).*')#去掉引号,以及?等无意义部分
+            p = re.compile('(\/item\/[^\?]+).*')  # 去掉引号,以及?等无意义部分
             # 5.1 用正则找info_box里面的链接 形如 "/item/123456" 这样的地方
             if len(tmp_link_list) == 0:
                 info_links.append('')
@@ -123,23 +132,22 @@ class BaiKeSpider(Spider):
         poly_dict = {}
         polysemy = sel.xpath('//ul[@class="polysemantList-wrapper cmn-clearfix"]/li')
 
-        first_poly_run=True
+        first_poly_run = True
 
         for poly_tmp in polysemy:
             poly_nodes = poly_tmp.re('>\s*(.*?)\s*<')
             poly_name = ""
             for poly_node in poly_nodes:
-                poly_name = "%s%s" % (poly_name, poly_node.replace('▪',''))
+                poly_name = "%s%s" % (poly_name, poly_node.replace('▪', ''))
             if first_poly_run is True:
-                poly_url=item['oid']
+                poly_url = item['oid']
                 poly_dict[poly_name] = parse.unquote(poly_url)
-                first_poly_run=False
+                first_poly_run = False
             else:
                 poly_url = poly_tmp.xpath('./a/@href').extract()
                 poly_url = " ".join(list(poly_url))
                 poly_url = poly_url.replace("#viewPageContent", "")
                 poly_dict[poly_name] = parse.unquote(poly_url)
-
 
         item['polysemy'] = poly_dict
 
@@ -148,7 +156,6 @@ class BaiKeSpider(Spider):
             item[key] = str(item[key]).replace('\n', '')  # 转换为字符串，好存进数据库
 
         yield item
-
 
         # extract links"/item..."注意链接此处还没有解码
         # 9.下一步的跳转，找到所有可以跳转的链接
@@ -165,3 +172,62 @@ class BaiKeSpider(Spider):
             request = Request(url=unquote_link, callback=self.parse, headers=self.headers, encoding='utf-8')
 
             yield request
+
+class PicturesSpider(Spider):
+    name = 'pic'
+    headers = {
+        'User-Agent':
+            'Mozilla/5.0 '
+            '(Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+    }
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'scrapyspider.pipelines.PictureUrlsPipeline': 1,
+            'scrapyspider.pipelines.PicturePipeline': 500
+        }
+    }
+    counts_and_oids={}#使用字典存储当前图片的序号和oid，以便命名
+    def __init__(self):
+        self.pics=pipelines.PictureUrlsPipeline()#sqls可调用pipeline的函数
+
+        # self.img_urls=[]
+
+    def start_requests(self):
+        count=1
+        maxWantID=400
+        while count <= maxWantID:
+            oid = parse.quote(self.pics.get_oid("entity_table", count))
+            self.counts_and_oids[oid]=str(count) #避免后期存储入文件夹时，斜杠被当做路径分割符号
+            page_url = "%s%s" % ("https://baike.baidu.com", oid)
+            print("page_url:",page_url)
+            request=Request(page_url, headers=self.headers,callback=self.parse, encoding='utf-8')
+            yield request
+            count+=1
+
+    def parse(self, response):
+        item=PicturesItem()
+        sel = Selector(response)
+
+        if len(response.xpath('//div[@class="summary-pic"]/a/img/@src'))==0:#判断该页是否有图片
+            item['image_urls']="none"
+            item['image_name']="none"
+        else:
+            img_url=sel.xpath('//div[@class="summary-pic"]/a/img/@src').extract()[0]
+            print('image_url appended:',img_url)
+            item['image_urls']=img_url
+
+            oid_pattern=re.compile('https://baike.baidu.com(.*)')#提取oid
+            m=oid_pattern.match(response.url)
+            oid=m.group(1)
+            img_count=self.counts_and_oids[oid]
+
+            item['image_name']="%s.%s"%(parse.unquote(oid.replace("/","_")),img_count)
+            print(item['image_name'])
+
+        # ticks = time.time()
+        # 当前时间
+
+
+        yield item
+
+
